759952de2c0150659400a79afd1e36b7a7221244
azriel <clickazr@gmail.com>
2025-01-28 18:29:56 +0200

    token

--- netflix/src/pages/MovieInformation.js
+++ netflix/src/pages/MovieInformation.js
@@ -2,10 +2,14 @@ import React, { useState, useEffect } from "react";
 import axios from "./axiosInstance";
 
 
-const MovieInformation = () => {
+const MovieInformation = ({token}) => {
     const[videoName, setVideoName] = useState('');
     const {id} = useParams();
     const[videoDescription, setvideoDescription] = useState('');
+    const[videoPoster, setvideoPoster]= useState('');
+    const[recoPosters, setrecoPosters] = useState([]);
+    const[recoIds, setrecoIds] = useState([]);
+    const[recoName, setrecoName] =useState([]);
 
 
 
@@ -17,16 +21,33 @@ const MovieInformation = () => {
                 "Content-Type": "application/json",
               },
         });
+
+        const reco = await axios.get(`/api/movies/${id}`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+        const posters = reco.recommendedmovies.map(movie => movie.poster);
+        const ids = reco.recommendedmovies.map(movie => movie.id);
+        const names =reco.recommendedmovies.map(movie => movie.name);
+        setrecoPosters(posters);
+        setrecoIds(ids);
+        setrecoName(names);
         setVideoName(response.data.name);
         setvideoDescription(response.data.name);
     } catch(error) {
-        serError("failed to load movie");
+        serError("failed to get movie info");
     }
 
     }
-}
+}, [id]
 
-    )
+    );
+return (
+    <div>
+        <h1>{}</h1>
+    </div>
 
+)
 
 }
